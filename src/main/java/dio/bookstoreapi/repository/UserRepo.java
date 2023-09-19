151a1e95ef95680947780f5d336fbd2cae5e67df
bruno-bahr <brunobahrbr@gmail.com>
2023-09-19 15:31:20 -0300

    Repository and Service classes update

---
+++ src/main/java/dio/bookstoreapi/repository/UserRepo.java
@@ -0,0 +1,9 @@
+package dio.bookstoreapi.repository;
+
+import dio.bookstoreapijwt.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepo extends JpaRepository<Users, Integer> {
+    UserDetails findByLogin(String login);
+}
---
+++ src/main/java/dio/bookstoreapi/service/AuthorizationService.java
@@ -0,0 +1,19 @@
+package dio.bookstoreapi.service;
+
+import dio.bookstoreapijwt.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    @Autowired
+    UserRepo userRepo;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepo.findByLogin(username);
+    }
+}
